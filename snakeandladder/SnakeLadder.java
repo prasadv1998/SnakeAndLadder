41e1f8a600cd713cc89169e6d7a816cb2cb7a2ac
prasadv1998 <prasadan1720@gmail.com>
2021-09-30 00:20:35 +0530

    Added: to print starting position of player

---
+++ snakeandladder/SnakeLadder.java
@@ -0,0 +1,8 @@
+package snakeandladder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int playerPosition = 0;
+		System.out.println("Player starting position:" +playerPosition);
+	}
+}
